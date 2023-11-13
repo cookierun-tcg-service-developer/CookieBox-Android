89c3e8fc10e1c86b748494b4a4be77bdac44274d
hyeonseo <s21035@gsm.hs.kr>
2023-11-13 09:23:41 +0900

    :zap: :: gradle convention plugin 적용

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,50 +1,12 @@
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.library)
-    alias(libs.plugins.kotlin)
-    alias(libs.plugins.kapt)
+    id("cookiebox.android.library")
 }
 
 android {
     namespace = "com.example.domain"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
 }
 
 dependencies {
     implementation(project(":core:data"))
 
-    implementation(libs.androidx.core)
-    implementation(libs.androidx.appcompat)
-
-    implementation(libs.google.material)
-    implementation(libs.google.hilt)
-    kapt(libs.google.hilt.compiler)
-
-    testImplementation(libs.test.junit)
-    androidTestImplementation(libs.test.android.junit)
-    androidTestImplementation(libs.test.espresso)
 }
