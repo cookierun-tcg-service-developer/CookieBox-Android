244759c73593b3ea6df8ebb35077d6ab317ee167
Cjsghkd <pjhomg5530@naver.com>
2023-11-15 17:06:09 +0900

    :heavy_plus_sign: compose plugin에 coil 종속성 추가

--- build-logic/src/main/java/cookiebox.android.compose.gradle.kts
+++ build-logic/src/main/java/cookiebox.android.compose.gradle.kts
@@ -1,6 +1,11 @@
 import com.example.cookiebox.android
 import com.example.cookiebox.configureComposeAndroid
+import com.example.cookiebox.libs
 
 android {
     configureComposeAndroid(this)
+}
+
+dependencies {
+    "implementation"(libs.findLibrary("libraries.coil").get())
 }
