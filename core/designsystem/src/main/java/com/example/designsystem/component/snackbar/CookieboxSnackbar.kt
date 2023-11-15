81000f7dec89307967b71d15b3175fcdd65a136b
Cjsghkd <pjhomg5530@naver.com>
2023-11-16 08:50:47 +0900

    :lipstick: CookieboxSnackbar 퍼블리싱

---
+++ core/designsystem/src/main/java/com/example/designsystem/component/snackbar/CookieboxSnackbar.kt
@@ -0,0 +1,44 @@
+package com.example.designsystem.component.snackbar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.designsystem.theme.CookieboxTheme
+
+@Composable
+fun CookieboxSnackbar(
+    modifier: Modifier = Modifier,
+    text: String,
+) {
+    Row(
+        modifier = modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(4.dp)
+            )
+            .padding(horizontal = 25.dp, vertical = 5.dp),
+        horizontalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = text,
+            style = CookieboxTheme.typography.textSmallR,
+            color = Color.Black
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CookieboxSnackbarPreview() {
+    CookieboxSnackbar(
+        text = "카드의 효과를 복사했습니다.",
+    )
+}
