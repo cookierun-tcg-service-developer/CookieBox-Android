86dc179d7efac7e3f29f220c84af761951e9bdf9
hyeonseo <s21035@gsm.hs.kr>
2023-11-20 17:37:23 +0900

    :sparkles: :: CardType에 따른 textColor 변경 로직

--- core/designsystem/src/main/java/com/example/designsystem/component/chip/CookieboxChip.kt
+++ core/designsystem/src/main/java/com/example/designsystem/component/chip/CookieboxChip.kt
@@ -1,6 +1,7 @@
 package com.example.designsystem.component.chip
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -47,13 +48,18 @@ fun CookieboxChip(
                 color = chipBackgroundColor(cardType = cardType),
                 shape = RoundedCornerShape(13.dp)
             )
+            .border(
+                width = 1.dp,
+                color = if (cardType == CardType.Stage) Color.Black else Color.Transparent,
+                shape = RoundedCornerShape(13.dp)
+            )
             .padding(horizontal = 15.5.dp, vertical = 3.dp),
         contentAlignment = Alignment.Center
     ) {
         Text(
             text = cardType.text,
             style = CookieboxTheme.typography.captionR,
-            color = Color.White,
+            color = chipTextColor(cardType = cardType),
         )
     }
 }
@@ -74,6 +80,15 @@ private fun chipBackgroundColor(cardType: CardType): Color {
         CardType.Trap -> CookieboxTheme.color.grayscale80
         CardType.Item -> CookieboxTheme.color.chipBlue
         CardType.Stage -> Color.White
+    }
+}
+
+@Composable
+private fun chipTextColor(cardType: CardType): Color {
+    return when (cardType) {
+        CardType.Cookie, CardType.Item -> Color.White
+        CardType.Trap -> CookieboxTheme.color.orange40
+        CardType.Stage -> Color.Black
     }
 }
 
